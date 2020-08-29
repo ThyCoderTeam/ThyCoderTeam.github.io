92bed3660850daddcfb01f0a481202e8615e1605
zj <zj05409@gmail.com>
2020-08-29 14:54:21 +0800

    battle city add enemies

--- parser.js
+++ parser.js
@@ -1,44 +1,70 @@
 // import constants from "./const";
 
 function getAtlasKey(symbol) {
-  return constants.TILE_TO_ATLAS[symbol];
+    return constants.TILE_TO_ATLAS[symbol];
 }
 
-function getSprites(levelData, game) {
-  return levelData
-    .split(/\r\n|\r|\n/g)
-    .join("")
-    .split("")
-    .map((tileSymbol, tileIndex) => {
-      var atlasKey = getAtlasKey(tileSymbol);
-      if (typeof atlasKey == "undefined") {
-        return null;
-      }
+function getSprites(levelData, game, enemyCount) {
+    if (enemyCount < 10) {
+        enemyCount = 10;
+    }
+    return levelData
+        .split(/\r\n|\r|\n/g)
+        .map((row, i) => {
+            if (i <= 3) {
+                return row.split('').map(( c, i, cs ) => {
+                    if (enemyCount > 0 && c === '.' && cs[i + 1] === '.') {
+                        enemyCount -= 1;
+                        return '!';
+                    }
+                    return c;
+                }).join('');
+            } else {
+                return row;
+            }
+        })
+        .join("")
+        .split("")
+        .map((tileSymbol, tileIndex) => {
+            var atlasKey = getAtlasKey(tileSymbol);
+            if (typeof atlasKey == "undefined") {
+                return null;
+            }
 
-      var args = [game, tileIndex, atlasKey];
-      return getTerrainSprite.apply(null, args);
-    })
-    .filter(sprite => {
-      return sprite != null;
-    });
+            var args = [game, tileIndex, atlasKey, game.groups[constants.ATLAS_TO_ENTITY_TYPE[atlasKey]]];
+            return getTerrainSprite.apply(null, args);
+        })
+        .filter(sprite => {
+            return sprite != null;
+        });
 }
 
-function getTerrainSprite(game, tileIndex, imageKey) {
-   const sprite =  game.physics.add.sprite(4 + xCoord(tileIndex), 4 + yCoord(tileIndex), "sprites", imageKey);
-  // game.physics.arcade.enable(sprite);
-  sprite.body.immovable = true;
-  sprite.extra = {
-    entityType: constants.ATLAS_TO_ENTITY_TYPE[imageKey]
-  };
-  return sprite;
+function getTerrainSprite(game, tileIndex, imageKey, group) {
+    let sprite = null;
+    if (imageKey === 'enemy_tank.png') {
+        sprite = group.create(4 + xCoord(tileIndex), 4 + yCoord(tileIndex), "enemy_tank");
+        sprite.extra = {
+            entityType: constants.ATLAS_TO_ENTITY_TYPE[imageKey]
+        };
+    } else {
+        sprite = group.create(4 + xCoord(tileIndex), 4 + yCoord(tileIndex), "sprites", imageKey);
+        sprite.body.immovable = true;
+        sprite.extra = {
+            entityType: constants.ATLAS_TO_ENTITY_TYPE[imageKey]
+        };
+    }
+
+    // const sprite =  game.physics.add.sprite(4 + xCoord(tileIndex), 4 + yCoord(tileIndex), "sprites", imageKey, group);
+    // game.physics.arcade.enable(sprite);
+    return sprite;
 }
 
 function xCoord(tileIndex) {
-  return (tileIndex % 26) * 8;
+    return (tileIndex % 26) * 8;
 }
 
 function yCoord(tileIndex) {
-  return Math.floor(tileIndex / 26) * 8;
+    return Math.floor(tileIndex / 26) * 8;
 }
 
 // export default getSprites;
